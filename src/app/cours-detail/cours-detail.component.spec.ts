03c1ba8e45747dfb34bf715e7e15039397854fec
nourbenyounes <118024722+nourbenyounes@users.noreply.github.com>
2025-01-19 16:45:21 +0100

    Update cours-detail.component.spec.ts

--- src/app/cours-detail/cours-detail.component.spec.ts
+++ src/app/cours-detail/cours-detail.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CoursDetailComponent } from './cours-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import HttpClientTestingModule
+import { CoursService } from './cours.service'; // Import your CoursService
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs'; // Import 'of' to return mock observable
+import { of } from 'rxjs'; // Import 'of' to return mock observables
 
 describe('CoursDetailComponent', () => {
   let component: CoursDetailComponent;
@@ -15,6 +17,7 @@ describe('CoursDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CoursDetailComponent],
+      imports: [HttpClientTestingModule], // Add HttpClientTestingModule here
       providers: [
         { provide: ActivatedRoute, useValue: mockActivatedRoute } // Provide the mock ActivatedRoute
       ]
