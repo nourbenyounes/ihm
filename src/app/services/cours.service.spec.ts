d73b238b47f0f336d6b0f3d5bd120d8e3479187e
nourbenyounes <118024722+nourbenyounes@users.noreply.github.com>
2025-01-19 16:01:52 +0100

    Update cours.service.spec.ts

--- src/app/services/cours.service.spec.ts
+++ src/app/services/cours.service.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { CoursService } from './cours.service';
 
 describe('CoursService', () => {
   let service: CoursService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CoursService);
   });
 
