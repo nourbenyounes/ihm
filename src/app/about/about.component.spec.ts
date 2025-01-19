7af2e8f397e40d1def610b79cc7b340b945066fa
nourbenyounes <118024722+nourbenyounes@users.noreply.github.com>
2025-01-19 17:30:13 +0100

    Update about.component.spec.ts

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms'; 
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
